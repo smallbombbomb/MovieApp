c4ecd1bd9a7529713e9e46ac7d639cb1bed14c9b
kimdonghyun <kdh8677@naver.com>
2019-12-28 00:04:57 +0900

    소스ㅅ정렬

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import './App.css';
 import Movie from './Movie';
 
 class App extends Component {
-//Render : ComponentWillMount() => render() => componentDidMount()
-//Update : componentWillReceiveProps() => shouldComponentUpdate() => componentWillUpdate() -> render() -> compoment
+  //Render : ComponentWillMount() => render() => componentDidMount()
+  //Update : componentWillReceiveProps() => shouldComponentUpdate() => componentWillUpdate() -> render() -> compoment
 
 
   state = {}
 
-  componentDidMount(){
+  componentDidMount() {
     this._getMovies();
   }
 
   _rendermovies = () => {
     const movies = this.state.movies.map((movie) => {
-      return <Movie 
-        title={movie.title_english} 
-        poster={movie.medium_cover_image} 
-        key={movie.id} 
-        genres={movie.genres}
-        synopsis={movie.synopsis}
+      return <Movie
+      title = {
+        movie.title_english
+      }
+      poster = {
+        movie.medium_cover_image
+      }
+      key = {
+        movie.id
+      }
+      genres = {
+        movie.genres
+      }
+      synopsis = {
+        movie.synopsis
+      }
       />
     })
     return movies
   }
-// asynchronous는 이전작업이 끝나야 다음작업 시작하는 형태가 X
-   _getMovies = async () => {
+  // asynchronous는 이전작업이 끝나야 다음작업 시작하는 형태가 X
+  _getMovies = async () => {
     //await모드 : callapi가 끝나기를 기다림, return상관없이
     const movies = await this._callApi();
     this.setState({
@@ -36,9 +48,9 @@ class App extends Component {
 
   _callApi = () => {
     return fetch('https://yts.lt/api/v2/list_movies.json?quality=3Dsort_by=rating')
-    .then(response => response.json())
-    .then(json => json.data.movies)
-    .catch(err => console.log(err))
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
   }
 
   render() {
@@ -49,6 +61,6 @@ class App extends Component {
       </div>
     );
   }
-  }
+}
 
 export default App;
